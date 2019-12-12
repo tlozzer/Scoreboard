9ba3985fdd057e32ad1702fff6db3104843e0e93
tlozzer1 <tlozzer@gmail.com>
2019-12-12 06:03:03 -0300

    Add navigation component to projeto

---
+++ app/src/main/java/br/com/zipvix/sportsscoreboard/ConfigFragment.kt
@@ -0,0 +1,24 @@
+package br.com.zipvix.sportsscoreboard
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ConfigFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_config, container, false)
+    }
+
+
+}
